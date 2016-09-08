0c7d3f3ae2cacb65da74a784b80b6987dc741a77
Nayoshi12 <nayoshi12gaming@gmail.com>
2016-09-08 11:19:14 -0400

    First Commit
    Remaking the whole thing, cleaning up bits and bobs.

---
+++ src/com/kyloka/voteTokenUI/main.java
@@ -0,0 +1,14 @@
+package com.kyloka.voteTokenUI;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class main extends JavaPlugin {
+	
+	public void onEnable(){
+		getLogger().info("VotenTokenUI Activated");
+	}
+	public void onDisable(){
+		getLogger().info("VoteTokenUI Deactivated");
+	}
+
+}
